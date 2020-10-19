e41279c4f88bd590d53f1311ee4b30b2dc5c06d7
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-19 11:28:15 -0400

    Removed input prompt from check

--- variables/__init__.py
+++ variables/__init__.py
@@ -14,7 +14,7 @@ def compiles():
 @check50.check(compiles)
 def check_12():
     """responds to 12"""
-    check50.run("./hello").stdin("12").stdout("Enter your age: 12\nRight now I'm 12 years old.\nNext year I'll be 13 years old.\nIn 10 years, I'll be 22!\nIn 50 years, I'll be 62! Wow!\n").exit()
+    check50.run("./hello").stdin("12").stdout("Right now I'm 12 years old.\nNext year I'll be 13 years old.\nIn 10 years, I'll be 22!\nIn 50 years, I'll be 62! Wow!\n"").exit()
 
 #@check50.check(compiles)
 #def rodrigo():
