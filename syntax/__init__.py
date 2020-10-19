454650250d3421816a816ef8ba6f6e826766004c
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-19 10:39:15 -0400

    added @check50.check(exists)

--- syntax/__init__.py
+++ syntax/__init__.py
@@ -5,6 +5,7 @@ def exists():
     """syntax.c exists"""
     check50.exists("syntax.c")
 
+@check50.check(exists)
 def syntax_check():
     """syntax is correct"""
     check50.run("make syntax").stdout("This is CS50AP!\n", regex=False).exit(0)
