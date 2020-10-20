da577ba99f6eda21ad0167fb2a4c5e9f78065698
Pete Lehman <lehman@cc8a5c337b.canisius.edu>
2020-10-19 21:44:29 -0400

    WORKING! (finally)

--- variables/__init__.py
+++ variables/__init__.py
@@ -14,7 +14,7 @@ def compiles():
 @check50.check(compiles)
 def check_12():
     """responds to 12"""
-    check50.run("./variables").stdin("12").stdout("Right now I'm 12 years old.\n").stdout("Next year I'll be 13 years old.\n").stdout("In 10 years, I'll be 22!\n").stdout("In 50 years, I'll be 62! Wow!\n").exit()
+    check50.run("./variables").stdin("12").stdout("Right now I'm 12 years old.\n").stdout("Next year I'll be 13 years old.\n").stdout("In 10 years, I'll be 22!\n").stdout("In 50 years, I'll be 62! Wow!\n").exit(0)
 
 #@check50.check(compiles)
 #def rodrigo():
