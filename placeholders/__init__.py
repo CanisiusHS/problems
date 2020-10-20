c792b90a35a1ba6865c7bbc0f81cf97035e87cf5
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-20 09:43:27 -0400

    .

---
+++ placeholders/__init__.py
@@ -0,0 +1,17 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """placeholders.c exists"""
+    check50.exists("placeholders.c")
+
+@check50.check(exists)
+def compiles():
+    """placeholders.c compiles"""
+    check50.c.compile("placeholders.c", lcs50=True)
+
+@check50.check()
+def cat_moon_25():
+    """Testing "cat" "moon" and "25.5"""
+    check.run("./placeholders").stdin("cat").stdin("moon").stdin("25.5").stdout("The cow jumped over the moon, 25.500000 times\n").exit()
