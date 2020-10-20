26c1af56b9a7d72a50652ab5030263298f1ed010
Eric Amodeo <ecamodeo@gmail.com>
2020-10-19 22:07:00 -0400

    types works
    using multiple line output

--- types/__init__.py
+++ types/__init__.py
@@ -13,7 +13,7 @@ def compiles():
 
 @check50.check(compiles)
 def test0():
-    """input of Dave yields Hello Dave, here is the data you entered..."""
+    """accepts Dave, 1994001, 34, 3.2, and 3.1415926535"""
     check50.run("./datatypes").stdin("Dave").stdin("34").stdin("3.2").stdin("1994001").stdin("3.1415926535").stdout("".join([
         "Hello Dave, here is the data you entered...\n",
         "Student ID: 1994001\n",
