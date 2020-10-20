2f9bc844e931c0aa4e076c4f1cafb2159404844f
Eric Amodeo <ecamodeo@gmail.com>
2020-10-19 20:34:31 -0400

    updated types

--- types/__init__.py
+++ types/__init__.py
@@ -7,11 +7,10 @@ def exists():
 
 @check50.check(exists)
 def compiles():
-    """datatypes.c compiles"""
+    """datatypes.c compiles."""
     check50.c.compile("datatypes.c", lcs50=True)
 
 @check50.check(compiles)
 def test0():
     """input of Dave yields Hello Dave, here is the data you entered..."""
     check50.run("./datatypes").stdin("Dave").stdout("Hello Dave, here is the data you entered...\n", regex=False).exit(0)
-    
