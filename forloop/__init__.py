55a434c04de91fb8f5bf2cc3ec8506270b5e65d7
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-20 14:43:40 -0400

    Created checks for for loop lab

---
+++ forloop/__init__.py
@@ -0,0 +1,18 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """forloop.c exists"""
+    check50.exists("forloop.c")
+
+@check50.check(exists)
+def compiles():
+    """forloop in variable:
+        pass.c compiles"""
+    check50.c.compile("forloop.c", lcs50=True)
+
+@check50.check()
+def right_sum():
+    """checks the math is right"""
+    check50.run("./forloop").stdout("55").exit(0)
