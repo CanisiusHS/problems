3f9f36daba22d4fceb7f3d0678d9ee656477d8d8
Eric Amodeo <ecamodeo@gmail.com>
2020-10-19 20:26:26 -0400

    started types

--- syntax/__init__.py
+++ syntax/__init__.py
@@ -6,6 +6,11 @@ def exists():
     check50.exists("syntax.c")
 
 @check50.check(exists)
+def compiles():
+    """syntax.c compiles"""
+    check50.c.compile("syntax.c", lcs50=True)
+
+@check50.check(exists)
 def syntax_check():
     """syntax is correct"""
-    check50.run("make syntax").stdout("This is CS50AP!\n", regex=False).exit(0)
+    check50.run("./syntax").stdout("This is CS50AP!\n", regex=False).exit(0)
---
+++ types/__init__.py
@@ -0,0 +1,17 @@
+import check50
+
+@check50.check()
+def exists():
+    """datatypes.c exists"""
+    check50.exists("datatypes.c")
+
+@check50.check(exists)
+def compiles():
+    """datatypes.c compiles"""
+    check50.c.compile("datatypes.c", lcs50=True)
+
+@check50.check(compiles)
+def test0():
+    """input of Dave yields Hello Dave, here is the data you entered..."""
+    check50.run("./datatypes").stdin("Dave").stdout("Hello Dave, here is the data you entered...\n", regex=False).exit(0)
+    
