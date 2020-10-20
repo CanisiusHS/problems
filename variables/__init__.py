0b1dd8f9db29a17e6c9ba382ae7aaab824410599
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-20 09:28:59 -0400

    .

--- variables/__init__.py
+++ variables/__init__.py
@@ -11,7 +11,7 @@ def compiles():
     """variables.c compiles"""
     check50.c.compile("variables.c", lcs50=True)
 
-@check50.check(compiles)
+@check50.check()
 def check_12():
     """responds to 12"""
     check50.run("./variables").stdin("12").stdout("Right now I'm 12 years old.\n").stdout("Next year I'll be 13 years old.\n").stdout("In 10 years, I'll be 22!\n").stdout("In 50 years, I'll be 62! Wow!\n").exit(0)
