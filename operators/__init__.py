bf1895b3547bcd5a176d3ef4611ae75b31f5f50c
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-20 10:32:19 -0400

    Check reject test

--- operators/__init__.py
+++ operators/__init__.py
@@ -16,7 +16,7 @@ def check_145():
     """responds to 145"""
     check50.run("./operators").stdin("145").stdout("a plus 3 is 148\n").stdout("a times 5 is 725\n").stdout("a divided by 4 is 36\n").stdout("a mod 4 is 1\n").stdout("a divided by 4.0 is really 36.250000\n").exit(0)
 
-@check50.check(compiles)
+@check50.check()
 def test_reject_foo():
     """rejects a non-numeric input of "foo" """
-    check50.run("./placeholders").stdin("foo").reject()
+    check50.run("./operators").stdin("foo").reject()
