8eec4e2c16ab42e8f9cc0c62e409aaf7a0583811
Eric Amodeo <ecamodeo@gmail.com>
2020-10-19 21:38:13 -0400

    .

--- types/__init__.py
+++ types/__init__.py
@@ -14,21 +14,4 @@ def compiles():
 @check50.check(compiles)
 def test0():
     """input of Dave yields Hello Dave, here is the data you entered..."""
-    out = check50.run("./datatypes").stdin("Dave").stdin("1994001").stdin("33").stdin("3.4").stdout()
-    check_input(out, open("types.txt").read())
-
-def check_input(output, correct):
-    if output == correct:
-        return
-
-    output = [line for line in output.splitlines() if line != ""]
-    correct = correct.splitlines()
-
-    help = None
-    if len(output) == len(correct):
-        if all(ol.rstrip() == cl for ol, cl in zip(output, correct)):
-            help = "Check each line to make sure they match properly."
-        elif all(ol[1:] == cl for ol, cl in zip(output, correct)):
-            help = "Check each line to make sure they match properly."
-
-    raise check50.Mismatch(correct, output, help=help)
+    check50.run("./datatypes").stdin("Dave").stdin("1994001").stdin("33").stdin("3.4").stdin("3.4")stdout()
