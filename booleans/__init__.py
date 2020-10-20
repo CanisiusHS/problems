dd94bc7827d26623b6ae4069d9085f512fe3ffda
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-20 11:30:26 -0400

    Forgot "" at the end of stdout

--- booleans/__init__.py
+++ booleans/__init__.py
@@ -14,34 +14,34 @@ def compiles():
 @check50.check()
 def check_95():
     """responds to 95"""
-    check50.run("./booleans").stdin("95").stdout("You get an A!\n).exit(0)
-#
-# @check50.check()
-# def check_85():
-    # """responds to 85"""
-    # check50.run("./booleans").stdin("85").stdout("You get a B!\n).exit(0)
-#
-# @check50.check()
-# def check_75():
-    # """responds to 75"""
-    # check50.run("./booleans").stdin("75").stdout("You get a C!\n).exit(0)
+    check50.run("./booleans").stdin("95").stdout("You get an A!\n").exit(0)
 
-# @check50.check()
-# def check_15():
-    # """responds to 15"""
-    # check50.run("./booleans").stdin("15").stdout("You need to work harder to pass this class!\n").exit(0)
+@check50.check()
+def check_85():
+    """responds to 85"""
+    check50.run("./booleans").stdin("85").stdout("You get a B!\n").exit(0)
 
-# @check50.check()
-# def check_150():
-    # """responds to 150"""
-    # check50.run("./booleans").stdin("150").stdout("You need to work harder to pass this class!\n").exit(0)
+@check50.check()
+def check_75():
+    """responds to 75"""
+    check50.run("./booleans").stdin("75").stdout("You get a C!\n").exit(0)
 
-#@check50.check()
-#def test_reject_foo():
-#    """rejects a non-numeric input of "foo" """
-#    check50.run("./booleans").stdin("foo").reject()
+@check50.check()
+def check_15():
+    """responds to 15"""
+    check50.run("./booleans").stdin("15").stdout("You need to work harder to pass this class!\n").exit(0)
 
-#@check50.check()
-#def test_reject_float():
-#    """rejects a floating-point number 55.5 """
-#    check50.run("./booleans").stdin("55.5").reject()
+@check50.check()
+def check_150():
+    """responds to 150"""
+    check50.run("./booleans").stdin("150").stdout("You need to work harder to pass this class!\n").exit(0)
+
+@check50.check()
+def test_reject_foo():
+   """rejects a non-numeric input of "foo" """
+   check50.run("./booleans").stdin("foo").reject()
+
+@check50.check()
+def test_reject_float():
+   """rejects a floating-point number 55.5 """
+   check50.run("./booleans").stdin("55.5").reject()
