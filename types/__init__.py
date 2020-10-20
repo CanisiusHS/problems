80ee331019c347c5eee48fe716ee9bb5952fbc8a
Eric Amodeo <ecamodeo@gmail.com>
2020-10-19 20:52:49 -0400

    .

--- types/__init__.py
+++ types/__init__.py
@@ -14,7 +14,7 @@ def compiles():
 @check50.check(compiles)
 def test0():
     """input of Dave yields Hello Dave, here is the data you entered..."""
-    out = check50.run("./datatypes").stdin("Dave").stdin("1994001").stdin("33").stdin("3.4").stdin("3.1413123973").stdout()
+    out = check50.run("./datatypes").stdin("Dave").stdin("1994001").stdin("33").stdin("3.4").stdout()
     check_input(out, open("types.txt").read())
 
 def check_input(output, correct):
