57578fd042d6ac89783d31f9f787ca184baa6913
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-23 08:49:18 -0400

    .

--- 1_1_syntax/__init__.py
+++ 1_1_syntax/__init__.py
@@ -11,7 +11,7 @@ def compiles():
     """syntax.c compiles"""
     check50.c.compile("syntax.c", lcs50=True)
 
-@check50.check(exists)
+@check50.check()
 def syntax_check():
     """syntax is correct"""
     check50.run("./syntax").stdout("This is CS50AP!\n", regex=False).exit(0)
