de52b5b02d7b03b8ebe995c24eef5f8b6aa6247d
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-23 08:52:20 -0400

    Corrected

--- 1_3_variables/__init__.py
+++ 1_3_variables/__init__.py
@@ -11,10 +11,10 @@ def compiles():
     """variables.c compiles"""
     check50.c.compile("variables.c", lcs50=True)
 
-@check50.check()
+@check50.check(compiles)
 def check_12():
     """responds to 12"""
-    check50.run("./variables").stdin("12").stdout("Right now I'm 12 years old.\n").exit()#.stdout("Next year I'll be 13 years old.\n").stdout("In 10 years, I'll be 22!\n").stdout("In 50 years, I'll be 62! Wow!\n").exit(0)
+    check50.run("./variables").stdin("12").stdout("Right now I'm 12 years old.\n").stdout("Next year I'll be 13 years old.\n").stdout("In 10 years, I'll be 22!\n").stdout("In 50 years, I'll be 62! Wow!\n").exit(0)
 
 @check50.check(compiles)
 def test_reject_foo():
