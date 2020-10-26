e898103ba10f8125b00e16c5ae5b9947f1f968ea
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-26 13:16:59 -0400

    Added bubble sort checks

---
+++ 3_2_bubble/__init__.py
@@ -0,0 +1,17 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """bubble.c exists"""
+    check50.exists("bubble.c")
+
+@check50.check(exists)
+def compiles():
+    """bubble.c compiles"""
+    check50.c.compile("bubble.c", lcs50=True)
+
+@check50.check(compiles)
+def check_72():
+    """responds to 72"""
+    check50.run("./bubble").stdout("0 1 2 3 4 5 6 7 8 9\n").exit()
