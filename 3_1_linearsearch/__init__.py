d2de3471f729358810964ff23d01e84aee4a1619
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-26 13:01:17 -0400

    Completed linear saearch check

---
+++ 3_1_linearsearch/__init__.py
@@ -0,0 +1,28 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """linear.c exists"""
+    check50.exists("linear.c")
+
+@check50.check(exists)
+def compiles():
+    """linear.c compiles"""
+    check50.c.compile("linear.c", lcs50=True)
+
+@check50.check(compiles)
+def check_72():
+    """responds to 72"""
+    check50.run("./linear").stdin("72").stdout("Found your number! Bingo!\n").exit()
+
+@check50.check(compiles)
+def check_72():
+    """responds to 2"""
+    check50.run("./linear").stdin("2").stdout("Sorry better luck next time!!\n").exit()
+
+
+@check50.check(compiles)
+def check_12():
+    """responds to 12"""
+    check50.run("./linear").stdin("Plumbus").reject()
