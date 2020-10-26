0e1b2e56e1ed31dd715bd97dacbdda9a6d406b6a
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-26 08:44:38 -0400

    Created typecasting checks

---
+++ 2_5_typecasting/__init__.py
@@ -0,0 +1,17 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """typecasting.c exists"""
+    check50.exists("exit.c")
+
+@check50.check(exists)
+def compiles():
+    """typecasting.c compiles"""
+    check50.c.compile("typecasting.c", lcs50=True)
+
+@check50.check(compiles)
+def check_gazorpazorp():
+    """responds to Gazorpazorp"""
+    check50.run("./typecasting")stdin("Gazorpazorp").stdout("Hb{psqb{psq").exit(0)
