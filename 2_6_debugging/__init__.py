978b67931ce34454977d8e8217ea250a8ec45a7c
Pete Lehman <lehman@Petes-MBP.canisius.org>
2020-10-26 10:25:36 -0400

    Debugging check created

---
+++ 2_6_debugging/__init__.py
@@ -0,0 +1,22 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """buggy.c exists"""
+    check50.exists("buggy.c")
+
+@check50.check(exists)
+def compiles():
+    """buggy.c compiles"""
+    check50.c.compile("buggy.c", lcs50=True)
+
+@check50.check(compiles)
+def check_1010101111():
+    """responds to 12"""
+    check50.run("./buggy").stdin("1010101111").stdout("687").reject()
+
+@check50.check(compiles)
+def check_12():
+    """responds to 12"""
+    check50.run("./buggy").stdin("12").reject()
