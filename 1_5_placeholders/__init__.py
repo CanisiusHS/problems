be8cf3c0020e4924ac60c1c85ddaff1a0ae4c364
Eric Amodeo <ecamodeo@gmail.com>
2020-10-31 17:25:03 -0400

    Fixed an expected output to more than 1 decimal place.

--- 1_5_placeholders/__init__.py
+++ 1_5_placeholders/__init__.py
@@ -14,7 +14,7 @@ def compiles():
 @check50.check(compiles)
 def cat_moon_25():
     """Testing "cow" "moon" and "25.5"""
-    check50.run("./placeholders").stdin("cow").stdin("moon").stdin("25.5").stdout("The cow jumped over the moon, 25.500000 times\n").exit()
+    check50.run("./placeholders").stdin("cow").stdin("moon").stdin("25.5").stdout("The cow jumped over the moon, 25.5 times\n").exit()
 
 @check50.check(compiles)
 def test_reject_foo():
