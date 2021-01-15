6366ddcc1f7ee7ef7bcb69201136dccea24601a4
Pete <plehman0@gmail.com>
2021-01-15 08:59:53 -0500

    Binary search checks

---
+++ 3_5_binary/__init__.py
@@ -0,0 +1,32 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """binary.c exists"""
+    check50.exists("binary.c")
+
+@check50.check(exists)
+def compiles():
+    """binary.c compiles"""
+    check50.c.compile("binary.c", lcs50=True)
+
+@check50.check(compiles)
+def check_50():
+    """Checks if 50 is in the list"""
+    check50.run("./binary").stdin("50").stdout("Not found!\n).exit()
+
+@check50.check(compiles)
+def check_14():
+    """Checks if 14 is in the list"""
+    check50.run("./binary").stdin("14").stdout("Found!\n).exit()
+
+@check50.check(compiles)
+def test_reject_foo():
+   """rejects a non-numeric input of "foo" """
+   check50.run("./binary").stdin("foo").reject()
+
+@check50.check(compiles)
+def test_reject_foo():
+   """rejects a non-int input of "15.5" """
+   check50.run("./binary").stdin("15.5").reject()
