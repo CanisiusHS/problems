5a7b5aa5d371f4fd65c849dc91a9f3cc8aa5be2d
Levi Nikirk <66690216+F4llenOW@users.noreply.github.com>
2021-02-26 09:02:44 -0500

    Update __init__.py

--- 3_8_models/__init__.py
+++ 3_8_models/__init__.py
@@ -5,11 +5,6 @@ import check50.c
 def exists():
    """monte_hall.c & simulate.c exists"""
    check50.exists("monte_hall.c", "simulate.c")
-
-@check50.check(exists)
-def compiles():
-   """monte_hall compiles"""
-   check50.c.compile("monte_hall.c", lcs50=True)
    
 @check50.check(exists)
 def compiles():
