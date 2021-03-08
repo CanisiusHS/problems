83ea2b4ea9e75fa08715f56d58e2f4320b58743a
Levi Nikirk <66690216+F4llenOW@users.noreply.github.com>
2021-03-08 07:05:26 -0500

    Create __init__.py

---
+++ 4_1_hexadecimal/__init__.py
@@ -0,0 +1,46 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+   """hexadecimal.c exists"""
+    
+@check50.check(exists)
+def compiles():
+   """hexadecimal.c compiles"""
+   check50.c.compile("hexadecimal.c", lcs50=True)
+  
+@check50.check(compiles)
+def test_reject_foo():
+    """rejects a non-numeric input of "foo" """
+    check50.run("./hexadecimal").stdin("foo").reject()
+
+@check50.check(compiles)
+def test_reject_float():
+    """rejects a non-int input of "15.5" """
+    check50.run("./hexadecimal").stdin("15.5").reject()
+   
+@check50.check(compiles)
+def check_FF():
+    """Checks if FF returns proper value"""
+    check50.run("./hexadecimal").stdin("FF").stdout("ff in decimal is 255\n").exit(0)
+    
+@check50.check(compiles)
+def check_1F():
+    """Checks if 1F returns proper value"""
+    check50.run("./hexadecimal").stdin("FF").stdout("1f in decimal is 241\n").exit(0)
+    
+@check50.check(compiles)
+def check_9A():
+    """Checks if 9A returns proper value"""
+    check50.run("./hexadecimal").stdin("9A").stdout("9a in decimal is 169\n").exit(0)
+    
+@check50.check(compiles)
+def check_a():
+    """Checks if a returns proper value"""
+    check50.run("./hexadecimal").stdin("a").stdout("a in decimal is 10\n").exit(0)
+    
+@check50.check(compiles)
+def check_9():
+    """Checks if 9 returns proper value"""
+    check50.run("./hexadecimal").stdin("9").stdout("9 in decimal is 9\n").exit(0)
