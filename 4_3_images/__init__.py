4c39c864db593dd38409b447ee463770c93cf4a5
Pete <plehman0@gmail.com>
2021-03-22 08:49:50 -0400

    Created image checks

---
+++ 4_3_images/__init__.py
@@ -0,0 +1,22 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+   """files exist"""
+   check50.exists("colorize.c", "smiley.bmp", "output.bmp")
+
+@check50.check(exists)
+def compiles():
+   """fileio.c compiles"""
+   check50.c.compile("colorize.c", lcs50=True)
+
+@check50.check(compiles)
+def test_reject_foo():
+    """rejects an input of "foo" """
+    check50.run("./colorize").stdout("Usage: colorize infile outfile\n").exit(1)
+
+@check50.check(compiles)
+def check_works():
+    """Checks if it copys the file"""
+    check50.run("./colorize smiley.bmp output.bmp").exit(0)
